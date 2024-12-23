2a9feca9fa6c648e0e3f679348e611bbaafb85a2
Arjo Chakravarty <arjoc@google.com>
2024-12-23 13:28:59 +0800

    Parametrize launch description

--- spawn_camera_gz/launch/spawn_camera_gz.py
+++ spawn_camera_gz/launch/spawn_camera_gz.py
@@ -1,26 +1,34 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import OpaqueFunction
 import launch.actions
 
 
-def generate_launch_description():
-    world_name = DeclareLaunchArgument(
-        'world_name',
-        default_value='panda_world'
-    )
+def launch_setup(context):
+    #config = LaunchConfiguration('world_name').perform(context) # Here you'll get the runtime config value
+
+    world_name = launch.substitutions.LaunchConfiguration('world_name')
     pkg_ros_gz_sim = get_package_share_directory('spawn_camera_gz')
     cmd_str= [
               "gz", "service", "-s",
-              f"/world/sim_world/create", "--reqtype", "gz.msgs.EntityFactory",
+              f"/world/{world_name.perform(context)}/create", "--reqtype", "gz.msgs.EntityFactory",
               "--req",
               f"'sdf_filename: \"{pkg_ros_gz_sim}/models/camera.sdf\"'",
               "--reptype", "gz.msgs.Boolean"]
 
-    return LaunchDescription([
-        ExecuteProcess(
-           cmd=cmd_str,#["gz", "topic", "--list"],
+    return [ExecuteProcess(
+          cmd=cmd_str,
           shell=True,
           output='both'
+        )]
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world_name',
+            default_value='sim_world'
         ),
+        OpaqueFunction(function = launch_setup),
     ])
